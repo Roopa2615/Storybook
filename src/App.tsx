17c38caca6195578c9db38b4cd8abd7f75fcd8f9
Roopashree <rupashree1011@gmail.com>
2024-07-01 17:54:32 +0530

    fixed styling issue of home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Box, useTheme } from '@mui/material';
 function App() {
   const theme = useTheme();
   return (
-    <Box sx={{width:'100%', height:'100%'}}>
+    <Box sx={{width:'100vw', height:'100vh', overflowX:'hidden', background:`${theme.palette.primary.main}`}}>
       <HomePage backgroundColor={theme.palette.primary.main}/>
     </Box>
   );
